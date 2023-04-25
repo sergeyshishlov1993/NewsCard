13f6ba72409fa8791deb8a2f2e60ef30b896ff65
sergey shishlov <shishlovsergey040593@gmail.com>
2023-04-25 12:43:53 +0300

    second

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <div class="card">
       <!-- c помошью интерполяции выводим динамически текуюшую дату на страницу -->
       <h2>Актуальные новости {{ now }}</h2>
-      <span> Открыто: {{ openRate }}</span>
+      <span>
+        <strong>Открыто: {{ openRate }} | Прочитано {{ readRate }}</strong>
+      </span>
     </div>
     <!-- используем компанент который мы импортировали и передаем в него ствойство prop, через цикл выводим содердимое массиыв на страницу -->
     <!-- с помошью директивы v-bind передаем props в компанент ребенка -->
@@ -12,7 +14,10 @@
       :items="item.titel"
       :id="item.id"
       v-model="item.isOpen"
-      @open-news="openRate++"
+      :wasRead="item.wasRead"
+      @open-news="openNews"
+      @read-news="readNews"
+      @unmark="removeNewsRead"
       v-for="item in news"
       :key="item.id"
     ></AppNew>
@@ -39,29 +44,75 @@ import AppNew from "./componets/AppNew";
 export default {
   data: () => ({
     openRate: 0,
+    readRate: 0,
     now: new Date().toLocaleDateString(), //создаем метод обькта который будет нам возврашать текуюшую дату в нужном флормате с помощью toLocaleDateString
     news: [
       {
         titel: "Какая то новость на этой странице ",
         id: 1,
         isOpen: false,
+        wasRead: false,
       },
       {
         titel: "Какая то новость на этой странице ",
         id: 2,
         isOpen: false,
+        wasRead: false,
       },
       {
         titel: "Какая то новость на этой странице ",
         id: 3,
         isOpen: false,
+        wasRead: false,
       },
 
       //создаем массив с какими то новостями которые мы планируем выводить на страницу
     ],
   }),
+  provide: () => {
+    return {
+      titel: "golova",
+      news: [
+        {
+          titel: "Какая то новость на этой странице ",
+          id: 1,
+          isOpen: false,
+          wasRead: false,
+        },
+        {
+          titel: "Какая то новость на этой странице ",
+          id: 2,
+          isOpen: false,
+          wasRead: false,
+        },
+        {
+          titel: "Какая то новость на этой странице ",
+          id: 3,
+          isOpen: false,
+          wasRead: false,
+        },
+
+        //создаем массив с какими то новостями которые мы планируем выводить на страницу
+      ],
+    };
+  },
   components: {
     AppNew,
+  },
+  methods: {
+    openNews() {
+      this.openRate++;
+    },
+    readNews(id) {
+      const idx = this.news.findIndex((news) => news.id === id); //для того что бы удалять кнопку у новости , у каждой новости сравнивает что индекс массива должен совпадать с индексом который мы передаем из ребенка компанента
+      this.news[idx].wasRead = true; //далее обрашаемся к массиву и к нужному индексу и меняем его значение с true на false
+      this.readRate++;
+    },
+    removeNewsRead(id) {
+      const news = this.news.find((news) => news.id === id);
+      news.wasRead = false;
+      this.readRate--;
+    },
   },
 };
 </script>
---
+++ src/componets/AppNewsList.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li v-for="item in news" :key="item.id">
+      {{ item.titel }}
+      <span v-if="item.wasRead">!!!</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  inject: ["titel", "news"],
+};
+</script>
